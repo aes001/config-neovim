3a280ab8e26aecd4778de04186d9dde84406baae
aes <edo.sunardi@gmail.com>
2025-05-24 13:16:59 +0100

    Add extra column to prevent warning and errors from overlapping with line number

--- lua/Racc/options.lua
+++ lua/Racc/options.lua
@@ -3,6 +3,11 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 vim.opt["tabstop"] = 4
 vim.opt["shiftwidth"] = 4
+vim.api.nvim_create_autocmd("BufWinEnter", {
+	callback = function()
+		vim.wo.signcolumn = "yes:1"
+	end,
+})
 vim.diagnostic.config({
   virtual_text = true,  -- show messages inline
   signs = true,         -- show E/W/etc in the sign column
