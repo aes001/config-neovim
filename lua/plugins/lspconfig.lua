5a74d098e9e48090ac11c854969bb5eb7a3a879c
aes <edo.sunardi@gmail.com>
2025-05-30 20:00:15 +0100

    Add key mappings for viewing definition, declaration, etc.

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -13,9 +13,19 @@ return {
 				local lspconfig = require("lspconfig")
 				local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+				local on_attach = function(_, bufnr)
+					local opts = { noremap = true, silent = true }
+
+					vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+					vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+				    vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) -- Hover docs
+					vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts) -- References
+				end
+
 				for _, lsp in ipairs(servers) do
 					lspconfig[lsp].setup({
 						capabilities = capabilities,
+						on_attach = on_attach,
 					})
 				end
 			end,
